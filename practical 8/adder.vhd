c17ae0b769e2508e9a4711398abd07e38fae6c48
David Lomax <d.e.lomax@me.com>
2018-07-18 22:05:52 +0100

    Adder with no bit growth.

---
+++ practical 8/adder.vhd
@@ -0,0 +1,37 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity adder is
+  generic
+  (
+    bits        : natural
+  );
+  port
+  (
+    clk         : in std_logic;
+    --
+    a_in        : in std_logic_vector(bits-1 downto 0);
+    b_in        : in std_logic_vector(bits-1 downto 0);
+    --
+    result_out  : out std_logic_vector(bits-1 downto 0)
+  );
+end entity;
+
+architecture rlt of adder is
+
+  signal a_u, b_u : unsigned(bits-1 downto 0);
+  signal sum_u    : unsigned(bits-1 downto 0);
+
+begin
+
+
+  a_u <= unsigned(a_in);
+  b_u <= unsigned(b_in);
+
+  sum_u <= a_u + b_u;
+
+  result_out <= std_logic_vector(sum_u);
+
+
+end architecture;
