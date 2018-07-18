5b46b663aaddd18c1873e0a92f4fa0b7ddc40059
David Lomax <d.e.lomax@me.com>
2018-07-18 22:43:22 +0100

    First commit of practical 9, counters.

---
+++ practical 9/counter.vhd
@@ -0,0 +1,33 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity counter is
+  generic
+  (
+    bits        : natural
+  );
+  port
+  (
+    clk         : in std_logic;
+    --
+    pulse_out   : out std_logic
+  );
+end entity;
+
+architecture rlt of counter is
+
+  signal counter_ur    : unsigned(bits-1 downto 0);
+
+begin
+
+  counter_proc : process(clk) is
+  begin
+    if rising_edge(clk) then
+      counter_ur <= counter_ur + 1;
+    end if;
+  end process;
+
+  pulse_out <= '1' when counter_ur = (counter_ur'range => '1') else '0';
+
+end architecture;
---
+++ practical 9/top_level.vhd
@@ -0,0 +1,34 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity toplevel is
+  port
+  (
+    clk       : in std_logic;
+    --  
+    switches  : in std_logic_vector(15 downto 0);
+    --
+    buttons   : in std_logic_vector(4 downto 0);
+    --
+    leds      : out std_logic_vector(15 downto 0)
+  );
+end entity;
+
+architecture rlt of toplevel is
+
+begin
+
+  adder_inst : entity work.counter
+  generic map
+  (
+    bits        => 8
+  )
+  port map 
+  (
+    clk         => clk,
+    --
+    pulse_out   => leds(0)
+  );
+
+end architecture;
