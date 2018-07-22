5207e3a7bf2d427f23c2823076fe9753bc7f445c
David Lomax <d.e.lomax@me.com>
2018-07-22 13:00:02 +0100

    First commit of practical 12, RAM.

---
+++ practical 12/ram.vhd
@@ -0,0 +1,45 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity ram is
+  generic
+  (
+    a_bits    : natural;
+    d_bits    : natural
+  );
+  port
+  (
+    clk       : in  std_logic;
+    --
+    d_in      : in  std_logic_vector(d_bits-1 downto 0);
+    wr_a_in   : in  std_logic_vector(a_bits-1 downto 0);
+    wr_en_in  : in  std_logic;
+    --
+    q_out     : out std_logic_vector(d_bits-1 downto 0);
+    rd_a_in   : in  std_logic_vector(a_bits-1 downto 0)
+  );
+end entity;
+
+architecture rlt of ram is
+
+  type ram_t is array (0 to (2**a_bits)-1) of std_logic_vector(d_bits-1 downto 0);
+  signal ram  : ram_t := (others => (others => '0'));
+
+  attribute ram_style : string;
+  attribute ram_style of ram : signal is "block";
+
+begin
+
+  ram_proc : process(clk) is
+  begin
+    if rising_edge(clk) then
+      if (wr_en_in = '1') then
+        ram(to_integer(unsigned(wr_a_in))) <= d_in;
+      end if;
+
+      q_out <= ram(to_integer(unsigned(rd_a_in)));
+    end if;
+  end process;
+
+end architecture;
---
+++ practical 12/top_level.vhd
@@ -0,0 +1,40 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity toplevel is
+  port
+  (
+    clk       : in std_logic;
+    --  
+    switches  : in std_logic_vector(15 downto 0);
+    --
+    buttons   : in std_logic_vector(4 downto 0);
+    --
+    leds      : out std_logic_vector(15 downto 0)
+  );
+end entity;
+
+architecture rlt of toplevel is
+
+begin
+
+  rom_i : entity work.ram
+  generic map
+  (
+    a_bits      => 8,
+    d_bits      => 8
+  )
+  port map 
+  (
+    clk         => clk,
+    --
+    d           => switches(7 downto 0),
+    wr_a        => switches(15 downto 8),
+    wr_en       => buttons(0),
+    --
+    q           => leds(7 downto 0),
+    rd_a        => switches(15 downto 8)
+  );
+
+end architecture;
