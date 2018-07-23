72fd3e5da70318a7e66837e8616381d05e3170d1
dlgb27 <d.e.lomax@me.com>
2018-07-23 22:47:13 +0100

    First commit of practical 14, FSMs.

---
+++ practical 14/combination_lock.vhd
@@ -0,0 +1,74 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity combination_lock is
+  port
+  (
+    clk         : in  std_logic;
+    --
+    clear_in    : in  std_logic;
+    buttons_in  : in  std_logic_vector(3 downto 0);
+    --
+    locked_out  : out std_logic
+  );
+end entity;
+
+architecture rlt of combination_lock is
+
+  type state_t is (locked, code1, code2, code3, unlocked);
+  signal state_r : state_t;
+
+begin
+
+  fsm_proc : process(clk) is
+  begin
+    if rising_edge(clk) then
+      case state_r is
+        when locked =>
+          locked_out <= '1';
+          --
+          if (buttons_in(2) = '1') then
+            state_r   <= code1;
+          end if;
+        when code1 =>
+          locked_out <= '1';
+          --
+          if (buttons_in(1) = '1') then
+            state_r   <= code2;
+          end if;
+          --
+          if (clear_in = '1') then
+            state_r   <= locked;
+          end if;
+        when code2 =>
+          locked_out <= '1';
+          --
+          if (buttons_in(2) = '1') then
+            state_r   <= code3;
+          end if;
+          --
+          if (clear_in = '1') then
+            state_r   <= locked;
+          end if;
+        when code3 =>
+          locked_out <= '1';
+          --
+          if (buttons_in(0) = '1') then
+            state_r   <= unlocked;
+          end if;
+          --
+          if (clear_in = '1') then
+            state_r   <= locked;
+          end if;
+        when unlocked =>
+          locked_out <= '0';
+          --
+          if (clear_in = '1') then
+            state_r   <= locked;
+          end if;
+      end case;
+    end if;
+  end process;
+
+end architecture;
---
+++ practical 14/top_level.vhd
@@ -0,0 +1,35 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity toplevel is
+  port
+  (
+    clk       : in std_logic;
+    --  
+    switches  : in std_logic_vector(15 downto 0);
+    --
+    buttons   : in std_logic_vector(4 downto 0);
+    --
+    leds      : out std_logic_vector(15 downto 0)
+  );
+end entity;
+
+architecture rlt of toplevel is
+
+  signal fifo_write, fifo_read    : std_logic;
+
+begin
+
+  fifo_i : entity work.combination_lock
+  port map 
+  (
+    clk         => clk,
+    --
+    clear_in    => buttons(0),
+    buttons_in  => buttons(4 downto 1),
+    --
+    locked_out  => leds(0)
+  );
+
+end architecture;
